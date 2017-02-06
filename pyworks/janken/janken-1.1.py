595b721364e7ec4adf982f93ebd055ddb9eafa31
zer0-317811 <317811.zer0@gmail.com>
2017-02-07 00:07:47 +0900

    remove pyworks

--- pyworks/janken/janken-1.1.py
+++
@@ -1,5 +0,0 @@
-
-import random
-data = ['goo','choki','pa']
-data_choice = random.choice(data)
-print(data_choice,data_choice,data_choice)
--- pyworks/janken/janken.py
+++
@@ -1,7 +0,0 @@
-import random
-#randomモジュールのインポート.この機能でランダムな動きを行う機能を使えるようにしている
-data = ['goo','choki','pa']
-#データの準備."="をつけて"data"という名前をつけておく
-data_choice = random.choice(data)
-# 2行目で用意したリストから無作為に でーた1つ選んでいる
-print(data_choice)
